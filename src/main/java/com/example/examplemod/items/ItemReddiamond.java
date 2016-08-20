d9fa4da64ea1d57ad1dd020b50b57d0a23963b07
BlackCritical <lvlup@t-online.de>
2016-08-20 05:01:32 +0200

    added diamond effect

--- src/main/java/com/example/examplemod/items/ItemReddiamond.java
+++ src/main/java/com/example/examplemod/items/ItemReddiamond.java
@@ -1,7 +1,12 @@
 package com.example.examplemod.items;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
 
 /**
  * Created by Kriller on 20.08.2016.
@@ -13,4 +18,9 @@ public class ItemReddiamond extends Item{
         this.setCreativeTab(CreativeTabs.MATERIALS);
     }
 
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
+        playerIn.addExperience(1000);
+        return super.onItemRightClick(itemStackIn, worldIn, playerIn, hand);
+    }
 }
